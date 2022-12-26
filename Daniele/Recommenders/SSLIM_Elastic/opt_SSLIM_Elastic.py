c89441c64fefb567b1503449002c12befa48c2b5
Daniele Cipollone <dan.cip00@gmail.com>
2022-12-26 20:18:37 +0100

    TF GPu setup

--- Daniele/Recommenders/SSLIM_Elastic/opt_SSLIM_Elastic.py
+++ Daniele/Recommenders/SSLIM_Elastic/opt_SSLIM_Elastic.py
@@ -34,6 +34,19 @@ from Data_manager.split_functions.split_train_validation_random_holdout import s
 from Evaluation.Evaluator import EvaluatorHoldout
 from sklearn.preprocessing import normalize
 
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    # Currently, memory growth needs to be the same across GPUs
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Memory growth must be set before GPUs have been initialized
+    print(e)
+
 URMv = dm.getURMviews()
 URMo = dm.getURMopen()
 ICMt = dm.getICMt()
