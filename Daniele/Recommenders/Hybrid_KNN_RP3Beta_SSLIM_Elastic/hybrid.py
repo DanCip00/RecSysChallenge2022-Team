6ee6e64734408b6113a88d73ca22976d55aec237
Daniele Cipollone <dan.cip00@gmail.com>
2022-12-31 01:16:39 +0100

    VM

--- Daniele/Recommenders/Hybrid_KNN_RP3Beta_SSLIM_Elastic/hybrid.py
+++ Daniele/Recommenders/Hybrid_KNN_RP3Beta_SSLIM_Elastic/hybrid.py
@@ -47,7 +47,8 @@ class SSLIMRP3BetaKNNRecommender(BaseItemSimilarityMatrixRecommender):
         self.slim_recommender = MultiThreadSLIM_SLIMElasticNetRecommender(URM_train=self.URM_train)
         self.sslim_BPR_recommender = SLIM_BPR_Cython(URM_train=self.urm_train_exp)
         self.most_viewed = TopPop(self.URM_train)
-
+        self.slim_combo = ItemKNNCustomSimilarityRecommender(self.URM_train)
+    
         self.most_viewed.fit()
         # {'topK': 69, 'alpha': 0.6854042891733674, 'beta': 0.2763471245555947, 'normalize_similarity': True} -> MAP 0.0282084
         self.rp3beta_recommender.fit( topK=69, alpha=0.6854042891733674, beta=0.2763471245555947, normalize_similarity=True )
@@ -57,13 +58,18 @@ class SSLIMRP3BetaKNNRecommender(BaseItemSimilarityMatrixRecommender):
         # {'topK': 51, 'epochs': 15, 'symmetric': True, 'sgd_mode': 'adam', 'lambda_i': 1e-05, 'lambda_j': 0.003215687724797301, 'learning_rate': 0.007114410195895492} -> MAP : 0.0078853
         self.sslim_BPR_recommender.fit(topK= 51, epochs=15,symmetric=True, sgd_mode = 'adam', lambda_i = 1e-05, lambda_j=0.003215687724797301, learning_rate = 0.007114410195895492)
 
+        alpha = 0.4
+        self.slim_combo.fit((1 - alpha) * self.slim_recommender.W_sparse + alpha* self.sslim_BPR_recommender.W_sparse)
+
+        
+
     def _compute_item_score(self, user_id_array, items_to_compute=None):
 
 
         item_weights = np.empty([len(user_id_array), self.URM_train.shape[1]])
         for i in range(len(user_id_array)):
 
-            interactions = len(self.urmv[user_id_array[i],:].indices)
+            interactions = len(self.URM_train[user_id_array[i],:].indices)
             if interactions >= 12 : #12 -> g. 7 
             
                 w1 = self.rp3beta_recommender._compute_item_score([user_id_array[i]])
@@ -72,12 +78,13 @@ class SSLIMRP3BetaKNNRecommender(BaseItemSimilarityMatrixRecommender):
                 w2 = self.slim_recommender._compute_item_score([user_id_array[i]])
                 w2 = w2 / np.linalg.norm(w2, 2)
 
-                item_weights = 0.6 * w2 + 0.4 * w1
+                item_weights[i,:] = 0.6 * w2 + 0.4 * w1
                 
 
             elif interactions >= 1:
-            
-                w1 = self.sslim_BPR_recommender._compute_item_score([user_id_array[i]])
+                
+                #w1 = self.sslim_BPR_recommender._compute_item_score([user_id_array[i]])
+                w1 = self.slim_combo._compute_item_score([user_id_array[i]])
                 item_weights[i,:] = w1 
 
             else:
